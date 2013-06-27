47b2c13c28bddfe41ebcf96a63cb9c5ffed5b8a4
ChrisMcKenzie <chris@keyboardsurfer.com>
2013-06-26 18:07:01 -0700

    Added color change to tags in posts

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,17 @@
 (function(){
   $('section .hashtags').each(function(){
     var tags = $(this).children();
-    console.log(tags);
+    tags.each(function(i, el){
+      var tag       = el,
+          color     = LightenDarkenColor('#ff6644', ((i+1) * 25));
+
+      if(i > 0) {
+        $(el).css({
+          'border-right': '3px solid ' + color
+        });
+      }
+    });
+
     tags.hover(function(){
       tags.css({
         opacity: 1
@@ -12,4 +22,23 @@
       });
     });
   });
+
+  function LightenDarkenColor(col, amt) {
+      var usePound = false;
+      if (col[0] == "#") {
+          col = col.slice(1);
+          usePound = true;
+      }
+      var num = parseInt(col,16);
+      var r = (num >> 16) + amt;
+      if (r > 255) r = 255;
+      else if  (r < 0) r = 0;
+      var b = ((num >> 8) & 0x00FF) + amt;
+      if (b > 255) b = 255;
+      else if  (b < 0) b = 0;
+      var g = (num & 0x0000FF) + amt;
+      if (g > 255) g = 255;
+      else if (g < 0) g = 0;
+      return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+  }
 })();
