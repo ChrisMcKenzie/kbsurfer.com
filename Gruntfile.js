ddece5d596e65e4dcdfa6734d462a06a9df62612
ChrisMcKenzie <chris@keyboardsurfer.com>
2013-10-16 18:18:49 -0700

    Major header revamp with cool sticky feature
    lots more grunt task updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,70 @@
 module.exports = function(grunt) {
   // Do grunt-related things in here
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
   grunt.initConfig({
+  	pkg: grunt.file.readJSON('package.json'),
+  	concat: {
+      css: {
+        src: ['public/css/*.css', '!public/css/*.min.css'],
+        dest: 'public/css/build/all.concat.css'
+      }
+    },
+  	uglify: {
+	    app: {
+	      options: {
+	        sourceMap: 'public/js/app.map.js',
+	        sourceMapPrefix: 2,
+	        sourceMappingURL: 'app.map.js'
+	      },
+	      files: {
+	        'public/js/app.min.js': ['public/js/*.js', '!public/js/*.min.js', '!public/js/*.map.js']
+	      }
+	    }
+	  },
     cssmin: {
 		  minify: {
 		  	keepSpecialComments: 1,
 		    expand: true,
-		    cwd: 'public/css/',
-		    src: ['*.css', '!*.min.css'],
+		    cwd: 'public/css/build',
+		    src: ['all.concat.css'],
 		    dest: 'public/css/',
 		    ext: '.min.css'
 		  },
-		  combine: {
-		    files: {
-		      'public/css/all.min.css': ['public/css/*.min.css']
-		    }
+		},
+		watch: {
+      css: {
+		    files: ['public/css/*.css', '!public/css/*.min.css'],
+		    tasks: ['concat', 'cssmin', 'clean'],
+		    options: {
+		      livereload: true,
+		    },
+		  },
+		  js: {
+		  	files: ['public/js/*.js', '!public/js/*.min.js', '!public/js/*.map.js'],
+		  	tasks: ['uglify'],
+		  	options: {
+		  		livereload: true,
+		  	}
+		  },
+		  jade: {
+		  	files: ['views/*'],
+		  	options: {
+		  		livereload: true
+		  	}
+		  }
+    },
+    clean: {
+		  build: {
+		    src: ['public/css/build']
 		  }
 		}
   });
 
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-  grunt.registerTask('default', ['cssmin'])
+  grunt.registerTask('default', ['concat', 'cssmin', 'uglify', 'clean'])
+  grunt.registerTask('css:minify', ['concat', 'cssmin', 'clean'])
 };
--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,41 @@
-(function(){
-  /*************Menu Actions************/
-  var button = $('.menu-button'),
-    wrapper = $('.menu-wrapper'),
-    header = $('header');
-
-  //Header management
-  header.waypoint('sticky', {
-    stuckClass: 'fixed',
-    offset: -50
-  });
-
+(function() {
+ 
+    var docElem = document.documentElement,
+        header = document.querySelector( 'header' ),
+        overlay = document.querySelector('.overlay'),
+        background = document.querySelector('.background'),
+        title = document.querySelector('.title'),
+        didScroll = false,
+        changeHeaderOn = 200;
+ 
+    var init = function() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 50 );
+            }
+        }, false );
+    };
+ 
+    function scrollPage() {
+        var sy = scrollY();
+        //console.log(sy);
+        if ( sy >= changeHeaderOn ) {
+      		console.log('change header');
+          classie.add( header, 'fixed' );
+        }
+        else {
+        	console.log('No change to header');
+          classie.remove( header, 'fixed' );
+        }
+        didScroll = false;
+    }
+ 
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+ 
+    init();
+ 
 })();
 
