7a64090b667f4427f3f13f31c5731bec1a88bf20
Christopher McKenzie <chris@kbsurfer.com>
2013-10-22 18:15:46 -0700

    added Google Analytics

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,7 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-3655066-3']);
+_gaq.push(['_trackPageview']);
+
 (function() {
  
     var docElem = document.documentElement,
@@ -37,6 +41,11 @@
  
     // And the kick-off!
     window.addEventListener( 'scroll', onScroll, false );
+    
+    // let's get our analytics on!
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
  
 })();
 
