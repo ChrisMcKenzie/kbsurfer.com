ff87776d151d873f3d56ba07e72094e744f37757
Christopher McKenzie <chris@kbsurfer.com>
2013-10-22 18:03:51 -0700

    removed js dependencies now using classList for class manipulation

--- public/js/app.js
+++ public/js/app.js
@@ -2,32 +2,40 @@
  
     var docElem = document.documentElement,
         header = document.querySelector( 'header' ),
+        classList = header.classList,
         ticking = false,
-        changeHeaderOn = 250;
+        changeHeaderOn = 250; // Distance in pixel to scroll from top before displaying fixed header
 
+    // Event Handler for Scrolling
     var onScroll = function() {
         update();
         requestTick();
     }
 
+    // Scroll Logic
     var update = function() {
         ticking = false;
+        // Get Yoffset otherwise set to top.
         var sy = window.pageYOffset || docElem.scrollTop;
-        //console.log(sy);
-        if ( sy >= changeHeaderOn ) {
-            classie.add( header, 'fixed' );
+        if(sy >= changeHeaderOn) {
+            // Show fixed header
+            classList.add('fixed');
         } else {
-            classie.remove( header, 'fixed' );
+            // Remove fixed header.
+            classList.remove('fixed');
         }
     }
 
     var requestTick = function() {
+        // Standard request animationFrame with ticking.
+        // This is the best way to do scroll handling w/o Jank.
         if(!ticking) {
             requestAnimationFrame(update);
         }
         ticking = true;
     }
  
+    // And the kick-off!
     window.addEventListener( 'scroll', onScroll, false );
  
 })();
